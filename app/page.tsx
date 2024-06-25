7fdbf8c3cfffc1aa789c6e8e4b22afdc265bf4f4
Carles Andres <carlos.andres.c@gmail.com>
2024-06-25 17:19:36 +0100

    Small improvements

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+    <main className="flex min-h-screen flex-col items-center justify-center py-2">
+      <div className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-5xl font-bold mb-8">Project title</h1>
         <ClientComponent />
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
