099a9b8aafc9d460b0f5cec70b963f6ec3ce447a
Carles Andres <carlos.andres.c@gmail.com>
2024-06-25 17:12:05 +0100

    Fix head import

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import ClientComponent from 'components/ClientComponent';
-import Head from 'next/head';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Project title',
+  description: 'A nice project description',
+  alternates: {
+    canonical: `/`,
+  },
+  openGraph: {
+    url: 'https://production_url.com',
+  },
+};
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>My project</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-5xl font-bold mb-8">Welcome to your project!</h1>
+        <h1 className="text-5xl font-bold mb-8">Project title</h1>
         <ClientComponent />
       </main>
     </div>
